60b19caf3f2e899c32ea0fc00d8fadf2501fc218
Suyash Bire <bire@mit.edu>
2022-07-05 16:42:32 -0400

    Plotting for histogram with initial y

--- pdf_xarray.py
+++ pdf_xarray.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 Omega_Enceladus = 5.3e-5  # Rotation rate [1/rad]
 rotation_period_Enceladus = 2*np.pi / Omega_Enceladus # [s]
 threshold_z = -1000
-ymax, ymin = 100000, -100000 # in meters
+
 
 fn = glob.glob("/net/fs08/d0/bire/mitgcm/enceladus_oceananigans/enceladus_expt16/run*/icy_moon_long_channel_particles.nc")
 
@@ -24,19 +24,27 @@ time = ds.time.values
 z = ds.z.values
 y = ds.y.values
 
-is_initial_y_in_range = (y[0,:] < ymax) & (y[0,:] > ymin)
 is_z_gt_threshold = (z > threshold_z)
-particles_cross_threshold_and_in_range = np.any(is_z_gt_threshold & is_initial_y_in_range, axis=0)
 
-z_particles_that_cross_threshold = z[:, particles_cross_threshold_and_in_range]
-
-time_particles_cross_threshold_and_in_range = np.argmax(z_particles_that_cross_threshold > threshold_z, axis=0)
-
-particles = particles[particles_cross_threshold_and_in_range]
-times = time[time_particles_cross_threshold_and_in_range]/rotation_period_Enceladus
+# the following ymin and ymax gives us three ranges: -400km to -100km, -100km to 100km, and 100km to 400km
+ymins, ymaxs = [-400000, -100000, 100000], [-100000, 100000, 400000] # in meters
 
 fig, ax = plt.subplots(1,1, figsize=(6,5))
-ax.hist(times)
+
+for ymin, ymax in zip(ymins, ymaxs):
+    is_initial_y_in_range = (y[0,:] < ymax) & (y[0,:] > ymin)
+    particles_cross_threshold_and_in_range = np.any(is_z_gt_threshold & is_initial_y_in_range, axis=0)
+
+    z_particles_that_cross_threshold = z[:, particles_cross_threshold_and_in_range]
+
+    time_particles_cross_threshold_and_in_range = np.argmax(z_particles_that_cross_threshold > threshold_z, axis=0)
+
+    particles_in_range = particles[particles_cross_threshold_and_in_range]
+    times_in_range = time[time_particles_cross_threshold_and_in_range]/rotation_period_Enceladus
+
+    ax.hist(times_in_range, histtype='step', label=f'{ymin/1000} to {ymax/1000}km')
+
 ax.set_xlabel('Transit time (rot. periods)')
 ax.set_ylabel('Number of particles')
-fig.savefig('transit_hist.png', bbox_inches='tight', dpi=150)
+ax.legend()
+fig.savefig(f'transit_hist.png', bbox_inches='tight', dpi=150)
